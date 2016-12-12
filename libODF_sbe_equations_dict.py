528ae29b723093d22a6ebe446636db5b31ac4486
jgum <jgum@ucsd.edu>
2016-12-11 21:01:30 -0500

    CR to C added
    Former-commit-id: 06ee9918ab3da7bc1dfb63e5a66e2ebf972b6ad7
    Former-commit-id: 5b54b12f644a7a68556a0584833918c123e427db [formerly 4a780854bdc927e5d6db5c6e646308ab12c832a7] [formerly f5b5f2bd5c8d0d96555475b3e90fd9766aa3e4cd [formerly 408b7b6dba88d827501ef8cfa7691c3667b5d8ed]]
    Former-commit-id: 6eec6aff0f98e4245640cee26cf9e27bc7a4788e [formerly 81a1a0e28c0e3c4b79c3ec90fcbdc5349a1f5096]
    Former-commit-id: ffc6a6ce6f423e5f2285215e831b2868aba47483
    Former-commit-id: f2cf73bfb9b11efd6ef7dfab12f511580607b7b7

--- libODF_sbe_equations_dict.py
+++ libODF_sbe_equations_dict.py
@@ -362,3 +362,33 @@ def fluoro_seapoint_dict(calib, signal):
     except:
         fluoro = round(signal,6)
     return fluoro
+
+def CR_to_C(cr):
+    """
+    Convert a given conductivity ratio to conductivity using constant from gsw library.
+
+    Given CR = C(S, t, p) / C(35, 15, 0) in (UNESCO, 1983) we adjust the equation to give
+    C(S, t, p) = CR / C(35, 15, 0). This gives us a conductivity that should be equivalent
+    to what we originally sampled - CHECK THIS?
+
+
+    Note that strictly speaking PSS-78 (Unesco, 1983) defines Practical
+    Salinity in terms of the conductivity ratio, R, without actually
+    specifying the value of C(35,15,0) (which we currently take to be
+    42.9140 mS/cm).
+
+    Input:
+    cr: conductivity ratio, either a single values or a list
+
+    Output:
+    output: a list or single value representing the measured conductivity
+    """
+
+    try:
+        output = []
+        for x in cr:
+            temp = x/42.9140
+            output.append(temp)
+    except:
+        output = cr/42.9140
+    return output
