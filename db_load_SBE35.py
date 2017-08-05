76c805ec1fc5eaabd77572cf59078531bfa01d65
Andrew Barna <abarna@gmail.com>
2017-08-04 14:58:10 -1100

    cleanup unused files from testing/experiments
    Former-commit-id: bc9307a19fd0c213b19e6bbd927e99067712f6dd
    Former-commit-id: 08897439daf3afbb0fa3115fcf3008c6e0ab43b3 [formerly 8766754b9d72e6ac90674843257c364575b8da15] [formerly 6fd5bfb4a9a80aceb212fbdefa9f7ac748ca6525 [formerly cb7f2e05caad7239ffa392318937fb738f97af43]]
    Former-commit-id: 1f9ca97b8fadc0b93fdb4fc272112c34169a08e8 [formerly 6d2cf98c58737746bbd143b1a30550d61ffe74ce]
    Former-commit-id: e75298ef747790626bea40594bd58dd619777184
    Former-commit-id: 4581ceaa847323844132e0fa6e4062b73503e76e [formerly 89cf4c499b2834d9a116d20f408c15455f4d36ef]
    Former-commit-id: 6620d8e8671144ff7d4f69300c87b9351a95a2ff

--- db_load_SBE35.py
+++
@@ -1,9 +0,0 @@
-import sys
-import db_load_scripts
-
-def main(argv):
-    with open(argv[0], 'r') as f:
-        db_load_scripts.load_SBE35(f, int(argv[1]), int(argv[2]))
-
-if __name__ == '__main__':
-    main(sys.argv[1:])
