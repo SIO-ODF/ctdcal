82d1389e6b8dc93c2c47a733ca0b95a7503130d8
Joseph Gum <jgum@ucsd.edu>
2017-08-16 15:53:09 -1000

    manual oxygen_offset changing
    Former-commit-id: 2ee910070b1f88f73652d7bc306127d55e647d38
    Former-commit-id: 6e96bcff6caec0608892f9953a44b65e550d8aeb [formerly 8473060f98577b9165da33de21f868c44cabe3b3] [formerly c0c9ca9e68fb44a866a0cc0593c14cb9155c069d [formerly bbc5aa32b85dfb3fb71d8cc81a85a485151a3436]]
    Former-commit-id: 630d86bbec204d335a996f32edd9bf231dbf4e94 [formerly 3a068f1df3e444b6e643b6894724e06cfbba4f3e]
    Former-commit-id: 6649a4cb329d857b201ca4c26c5e44f30fba60e9
    Former-commit-id: 70a8a37c719a546790608198711bd1c27a94b845 [formerly 83370d69ef430d3d1b0c9eefd7ff617f73bb0d23]
    Former-commit-id: 1b8d72b369e5be1db8efbcf0911e25e58d9cc79d

---
+++ oxygen_offset.py
@@ -0,0 +1,47 @@
+import pandas as pd
+import libODF_merge_codes as merge
+import numpy as np
+import io
+
+qual_codes_filepath = f'/Users/jgum/work_code/cruises/P06_2017/quality_codes/'
+cruise_dir = f'/Users/jgum/work_code/cruises/P06_2017/'
+log_dir = f'{cruise_dir}ctd_proc_rewrite/data/logs/quality_code/all/'
+
+file_ssscc = f'{cruise_dir}ssscc.csv'
+bottle_file= f'{qual_codes_filepath}320620170703_hy1.csv'
+
+### compile bottle trips into one file, then merge with odf_db bottle file
+#df_bottle = merge.prelim_ctd_bottle_df(file_ssscc, bottle_file)
+### compute residuals for plotting
+#merge.ctd_residuals_df(df_bottle)
+df_bottle = merge.load_exchange_bottle_file(bottle_file)
+df_bottle = merge.merge_ctd_codes(log_dir, df_bottle)
+#df_bottle = merge.get_qc_all_from_df(df_bottle)
+
+df_bottle['CTDOXY'] = df_bottle['CTDOXY'] - 2.2
+df_bottle['CTDOXY_FLAG_W'] = df_bottle['CTDOXY_FLAG_W'] + 1
+df_bottle = df_bottle.rename(index=str, columns={'CTDCOND1_FLAG_W': 'CTDSAL_FLAG_W'})
+
+df_bottle.to_pickle('data/values_and_codes.pkl')
+print(f'Finished pickling bottle file')
+all_casts = []
+with open(file_ssscc, 'r') as filename:
+    all_casts = [line.strip() for line in filename]
+
+for ssscc in all_casts:
+    lines = ''
+    with open(f'{cruise_dir}ctd_proc_rewrite/data/scratch_folder/pressure/{ssscc}_ct1.csv', 'r') as f:
+        for x in range(0,14):
+            lines += f.readline()
+    df = pd.read_csv(f'{cruise_dir}ctd_proc_rewrite/data/scratch_folder/pressure/{ssscc}_ct1.csv',
+    skiprows=[0,1,2,3,4,5,6,7,8,9,10,11,13], skipfooter=1, engine='python')
+    df['CTDOXY'] = df['CTDOXY'] - 2.2
+    df['CTDOXY_FLAG_W'] = df['CTDOXY_FLAG_W'] + 1
+    lines += df.to_string(header=False, index=False, formatters=[lambda x: str(x)+',',
+    lambda x: str(x)+',',lambda x: str(x)+',',lambda x: str(x)+',',lambda x: str(x)+',',
+    lambda x: str(x)+',',lambda x: str(x)+',',lambda x: str(x)+',',lambda x: str(x)+',',
+    lambda x: str(x)+',',lambda x: str(x)+',',lambda x: str(x)+',',lambda x: str(x)+',',
+    lambda x: str(x)+',',lambda x: str(x)+',',lambda x: str(x)])
+    lines += f'\nEND_DATA\n'
+    with open(f'{cruise_dir}ctd_proc_rewrite/data/scratch_folder/pressure/{ssscc}_ct1.csv', 'w') as f:
+        f.write(lines)
