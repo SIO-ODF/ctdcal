35fed1a08a100cfb756267b534caaf26a1e26ed8
Joseph Gum <jgum@ucsd.edu>
2017-09-11 14:36:12 -0700

    updated oxygen_offset for nbp1707
    Former-commit-id: 7e5b48553cf07d97719a0fe8374a1fb6694a9e49
    Former-commit-id: 02540143f1873bdbc5b0842551cc7c832e439469 [formerly 092cc026d5a4639d602e5351be951368c0bbb3f4] [formerly d1f71230df4e5db3c7a630e34a19714d3c238014 [formerly 937c655368c27b8e4f4806fcc42aa752075e0892]]
    Former-commit-id: 1e66a8d03d180c5ffc2673cd30eb64989581e638 [formerly 2b0f1218ffd716325770359981a875a1748feea8]
    Former-commit-id: 45f0ccb8e0c169d5d6ff4b6d027e17fa5b2f3e48
    Former-commit-id: a17869e61e86c1e51228e37e156fcb3743d5040d [formerly 756b1af19865ea2007cfc14a5043f6b3af99a633]
    Former-commit-id: 933f3be2e2efe27164c14741bdca1dc3c9956438

--- oxygen_offset.py
+++ oxygen_offset.py
@@ -3,12 +3,12 @@ import libODF_merge_codes as merge
 import numpy as np
 import io
 
-qual_codes_filepath = f'/Users/jgum/work_code/cruises/P06_2017/quality_codes/'
-cruise_dir = f'/Users/jgum/work_code/cruises/P06_2017/'
+qual_codes_filepath = f'/Users/jgum/work_code/cruises/NBP1707/quality_codes/'
+cruise_dir = f'/Users/jgum/work_code/cruises/NBP1707/'
 log_dir = f'{cruise_dir}ctd_proc_rewrite/data/logs/quality_code/all/'
 
 file_ssscc = f'{cruise_dir}ssscc.csv'
-bottle_file= f'{qual_codes_filepath}320620170703_hy1.csv'
+bottle_file= f'{qual_codes_filepath}320620170820_hy1.csv'
 
 ### compile bottle trips into one file, then merge with odf_db bottle file
 #df_bottle = merge.prelim_ctd_bottle_df(file_ssscc, bottle_file)
