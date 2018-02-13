fixed converting raw sbe hex to sci units
Former-commit-id: 2dd0a6b042b8aa6eb1a07ffb5197db71234c48a7
Former-commit-id: 609cade5748ac8d8198266bb1d894861ee35106f [formerly a3d71b770cd23f94a273e1d4d4ab2502bc7d9fc1] [formerly 2e9aceed169da82e65d8295c5bd04046598fb091 [formerly 10e6c2d4bb6e8cd0918881f6ab89d01bcc94099b]]
Former-commit-id: 8f778c5332c997ae561133d49d515ed723d2b056 [formerly 58baf7a5ff91eb523455ac2df1f64cec508373df]
Former-commit-id: f9408e968b921a632b65749f8dd825171319740f
Former-commit-id: 91acb4ee271d2a7482d8720526e38c17ec897298 [formerly b73962adcb8223d986913ae81f8357b97ef582f8]
Former-commit-id: 6a4c75a89afe8ee1fdfebcac1b2c46694b588348

ctdcal/sbe_reader.py
@@ -72,6 +72,7 @@ class SBEReader():
         #this uses numpy magic to just "apply" the needed operations to the correct part of the array all in one go
         measurements[:,:num_frequencies] = measurements[:,:num_frequencies] / 256
         measurements[:,num_frequencies:num_frequencies+num_voltages] = (5 * (1 - (measurements[:,num_frequencies:num_frequencies+num_voltages] / 4095)))
+        measurements = measurements[:,0:-1] #remove after applying _parse_scans_meta in here
         #print(measurements.shape)
         return measurements
 
@@ -572,11 +573,17 @@ class SBEReader():
 
     @property
     def parsed_scans(self):
-        try:
-            return self._parse_scans
-        except AttributeError:
-            self._parse_scans = np.concatenate((self._parse_scans(), self._parse_scans_meta().reshape(self._parse_scans_meta().size,1)), axis = 1)
-            return self._parse_scans
+        """
+        Wrapper for _parse_scans and _parse_scans_meta. Returns np.ndarray
+        """
+        # try:
+        #     return self._parse_scans
+        # except AttributeError:
+        #     self._parse_scans = np.concatenate((self._parse_scans(), self._parse_scans_meta().reshape(self._parse_scans_meta().size,1)), axis = 1)
+        #     return self._parse_scans
+        #import pdb; pdb.set_trace()
+        return np.concatenate((self._parse_scans(), self._parse_scans_meta().reshape(self._parse_scans_meta().size,1)), axis = 1)
+        #return self._parse_scans
 
 
     def parsed_config(self):