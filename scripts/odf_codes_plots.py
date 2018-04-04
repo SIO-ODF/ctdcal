ececb6d0ecaa464f6b48b4147cbff5b7dd8e0ca7
Joseph Gum <jgum@ucsd.edu>
2018-04-03 15:02:57 -0900

    updated for nbp1802
    Former-commit-id: f85a901371e2432dddbea93c240ba791c144b07a
    Former-commit-id: 335a1165cb898fb61213c4d315ed00725cb17e8e [formerly 5509ba9068fb82e6d190e39dfdcf01710b63e240] [formerly 018822cac6bae6d754174f2334a583501466150d [formerly 59ac95dd498b3ce311f3c56ab9ed8c50450767bd]]
    Former-commit-id: 7a87bd8c042f2017579ee0759654944d8174e2e6 [formerly 58484c851f3f9eda45a5b4801aa08aa3e90658b9]
    Former-commit-id: 6a0604e00ab348f84f5662c33b1ba16a70d7fbc9
    Former-commit-id: 7e7db5c8f627cccae4df0239d295c4f35940a2f2 [formerly c9a604e37a6502b8626e55231788b24b758f9057]
    Former-commit-id: 752710f17dc538be7544c52966948cf334c35b6f

--- scripts/odf_codes_plots.py
+++ scripts/odf_codes_plots.py
@@ -8,12 +8,13 @@ import pandas as pd
 #import numpy as np
 import matplotlib.pyplot as plt
 
-qual_codes_filepath = f'/Users/jgum/work_code/cruises/P06_2017/quality_codes/'
-cruise_dir = f'/Users/jgum/work_code/cruises/P06_2017/'
-log_dir = f'{cruise_dir}ctd_proc_rewrite/data/logs/quality_code/all/'
+expocode = f'320620180309'
 
-file_ssscc = f'{cruise_dir}ssscc.csv'
-bottle_file= f'{qual_codes_filepath}320620170703_hy1.csv'
+qual_codes_filepath = f'data/quality_codes/'
+log_dir = f'data/logs/quality_code/all/'
+
+file_ssscc = f'data/ssscc.csv'
+bottle_file= f'{qual_codes_filepath}{expocode}_hy1.csv'
 
 ### compile bottle trips into one file, then merge with odf_db bottle file
 df_bottle = merge.prelim_ctd_bottle_df(file_ssscc, bottle_file)
