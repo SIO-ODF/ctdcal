b990461d471562d4a96d24359966acabe0db763c
Joseph Gum <jgum@ucsd.edu>
2018-04-14 18:48:13 -0700

    first pass on sbe conversions to numpy
    Former-commit-id: 22b2060b93ed1dbbf9589655735eec9fb061f249
    Former-commit-id: 5d3a1562c9caba496c2d2bf65ec7c023b0173865 [formerly 279a8e7f36a27fe27cf397e04bdb7e55dd41b9ca] [formerly 1b766fd57eb6f1ea803cb988a1eb72b90a8a03d7 [formerly 07b8b9857485692ed3553be0bfefd46b745ee441]]
    Former-commit-id: 3317925d40c5408a04901b0c145b934ec0051b6d [formerly db43511315bf4060df60e04b823fd52989ccca88]
    Former-commit-id: 5378a004992b7e1e1d92140feab4406840df17a0
    Former-commit-id: 0249cca015796063b368acd75be3581b4c1785ff [formerly 30984cf51b5f9b6ca85fd46a82526a7603785d8a]
    Former-commit-id: f21258057d61284df9c6edf96c7970fb48f53598

--- ctdcal/sbe_equations_dict.py
+++ ctdcal/sbe_equations_dict.py
@@ -103,14 +103,178 @@ def temp_its90(calib, f):
 #     to not producing a value where 0 is used, ending in a -273.15 value being output
     return ITS90
 
+def sbe9(calib, f, t):
+    """NOT TESTED
+
+    SBE/STS(?) equation for converting SBE9 pressure frequency to pressure.
+    SensorID: 45
+
+    Parameters
+    ----------
+    calib : dict
+        T1: coefficient
+        T2: coefficient
+        T3: coefficient
+        T4: coefficient
+        T5: not used
+        C1: coefficient
+        C2: coefficient
+        C3: coefficient
+        D1: coefficient
+        D2: coefficient
+        AD590M: used in digiquartz temeperature correction
+        AD590B: used in digiquartz temperature correction
+
+    f : array-like
+        f is sensor frequency (usually between 30kHz and 42kHz)
+    t : array-like
+        t is sensor integer from the digiquartz temperature probe
+
+    Returns
+    -------
+    p : array-like
+        p is pressure in decibar
+    """
+    #Equation expecting pressure period in microseconds, so divide f by 1,000,000.
+    uf = f / 1000000
+    t = (calib['AD590M'] * int(t)) + calib['AD590B']
+    T0 = calib['T1'] + calib['T2']*t + calib['T3']*np.power(t,2) + calib['T4']*np.power(t,3)
+    w = 1-T0*T0*f*f
+    pressure = (0.6894759*((calib['C1']+calib['C2']*t+calib['C3']*t*t)*w*(1-(calib['D1']+calib['D2']*t)*w)-14.7))
+    return np.around(pressure,4)
+
+def sbe4c(calib, F, t, p):
+    """NOT TESTED
+
+    SBE equation for converting SBE4C frequency to conductivity. Calculates mS/cm
+
+    SensorID: 3
+
+    Parameters
+    ----------
+    calib : dict
+        G : coefficient
+        H : coefficient
+        I : coefficient
+        J : coefficient
+        CPcor : coefficient (nominal)
+        CTcor : coefficient (nominal)
+    F : array-like
+        F is instrument frequency
+    t : array-like
+        t is temperature (ITS-90 degrees C)
+    p : array-like
+        p is pressure (decibars)
+
+    Returns
+    -------
+    c : array-like
+        c is conductivity in mS/cm
+    """
+    f = F/1000
+    c = ((calib['G'] + calib['H'] * np.power(f,2)
+                     + calib['I'] * np.power(f,3)
+                     + calib['J'] * np.power(f,4))
+                    / (1 + calib['CTcor'] * t + calib['CPcor'] * p))
+    #S/m to mS/cm
+    c = c * 0.1
+    c = round(c,5)
+    return c
+
+def sbe43(calib, P, K, T, S, V):
+    """NOT TESTED
+    IS THIS IN ITS-90? DOES IT MATTER?
+    SBE equation for converting SBE43 engineering units to oxygen (ml/l).
+
+    SensorID: 38
+
+    Parameters
+    ----------
+
+    calib : dictionary
+        calib is a dict holding Soc, Voffset, Tau20, A, B, C, E
+    P : array-like
+        P is pressure in decibars
+    K : array-like
+        K is temperature in Kelvin
+    T : array-like
+        T is temperature in Celcius
+    S : array-like
+        S is Practical Salinity Units, PSS-78
+    V : array-like
+        V is voltage from instrument
+
+    Returns
+    -------
+    oxygen : array-like
+        oxygen is the oxygen measurement in ml/l
+
+    Original equation from calib sheet dated 2014:
+    Oxygen (ml/l) = Soc * (V + Voffset) * (1.0 + A * T + B * T + C * T ) * OxSol(T,S) * exp(E * P / K)
+    """
+
+    oxygen = np.around(calib['Soc'] * (V + calib['offset'])
+              * (1.0 + calib['A'] * T + calib['B'] * np.power(T,2) + calib['C'] * np.power(T,3) )
+              * OxSol(T,S)
+              * np.exp(calib['E'] * P / K),4)
+    return oxygen
+
+def OxSol_New(T,S):
+    """Should work with sbe43(), should be depreciated in favor of gsw in the future
+
+    Eq. 8 from Garcia and Gordon, 1992.
+    Harded coded to do ml/l for now. If requeste, add in new mode for ug/l.
+
+    Parameters
+    ----------
+    T : array-like
+        T is the ITS-90 temperature in Celcius
+    S : array-like
+        S is the Practical Salinity, PSS-78
+    """
+
+    x = S
+    y = np.log((298.15 - T)/(273.15 + T))
+
+    """umol/kg coefficients
+    a0 =  5.80871
+    a1 =  3.20291
+    a2 =  4.17887
+    a3 =  5.10006
+    a4 = -9.86643e-2
+    a5 =  3.80369
+    b0 = -7.01577e-3
+    b1 = -7.70028e-3
+    b2 = -1.13864e-2
+    b3 = -9.51519e-3
+    c0 = -2.75915e-7
+    """
+
+    """ml/l coefficients"""
+    a0 = 2.00907
+    a1 = 3.22014
+    a2 = 4.05010
+    a3 = 4.94457
+    a4 = -2.56847e-1
+    a5 = 3.88767
+    b0 = -6.24523e-3
+    b1 = -7.37614e-3
+    b2 = -1.03410e-2
+    b3 = -8.17083e-3
+    c0 = -4.88682e-7
+
+    O2sol = np.exp(a0 + y*(a1 + y*(a2 + y*(a3 + y*(a4 + a5*y)))) + x*(b0 + y*(b1 + y*(b2 + b3*y)) + c0*x))
+    return O2sol
+
 
 def OxSol(T,S):
     """Eq. 8 from Garcia and Gordon, 1992.
     Harded coded to do ml/l for now. If requeste, add in new mode for ug/l.
 
-    Inputs:
-    T = ITS-90 Temperature
-    S = Practical Salinity
+    Parameters
+    ----------
+    T : Temperature
+    S : Practical Salinity
     """
 
     x = S
@@ -156,6 +320,36 @@ def oxy_hysteresis_voltage(calib, voltage, scan_window=48):
     calib: a dict holding H1, H2, H3, Voffset
     voltage: a sequence of engineering voltages
     scan_window: an int for scans to skip between, default 48 scans at 24Hz OR 2 seconds
+    '''
+    output = []
+
+    for i, x in enumerate(voltage):
+        if i == 0:
+            continue
+
+        D = 1 + calib['H1']*(exp(P(i)/calib['H2']) - 1)
+        C = exp(-1 * ())
+
+    return output
+
+def sbe43_hysteresis_voltage(calib, voltage):
+    '''NOT TESTED NOT FINISHED
+
+    SBE equation for computing hysteresis from raw voltage in relation to SBE43.
+    Must be run before oxy_dict.
+    Because of looking backwards, must skip i = 0.
+
+    Parameters
+    ----------
+    calib : dict
+        calib is a dict holding H1, H2, H3, Voffset
+    voltage : array-like
+        voltage is a sequence of engineering voltages
+
+    Returns
+    -------
+    output : array-like
+        output is the
     '''
     output = []
 
@@ -252,30 +446,6 @@ def cond_dict(calib, F, t, p, units='mS'):
         Conductivity = Conductivity * 0.1
         Conductivity = round(Conductivity,5)
     return Conductivity
-
-
-def sp_dict(c, t, p):
-    """Wrapper of SP_from_C from gsw library.
-    Take in non-numpy data, format to numpy array, then run through.
-    Goal to eventually deprecate this.
-
-    Inputs:
-    c: array, Conductivity in mS/cm
-    t: array, in-situ temp in Celcius
-    p: array, sea pressure
-
-    Output:
-    SP: array, practical salinity (PSS-78)
-
-    """
-    c = np.array(c)
-    t = np.array(t)
-    p = np.array(p)
-
-    SP = gsw.SP_from_C(c,t,p)
-
-    return SP
-
 
 def pressure_dict(calib, f, t):
     """SBE/STS(?) equation for converting pressure frequency to temperature.
