2063a7e0ae2400d56deb3a27bb0ba31cf08e9e49
Joseph Gum <jgum@ucsd.edu>
2018-05-07 19:50:20 -0600

    adjusted to make final pkl file for upload
    Former-commit-id: 561f2f8d95c13531206d36266797e142b1678527
    Former-commit-id: ec7259b24b8e8f71dbf70c6bf29939990102226e [formerly 16fc8a00422460188d581153457b0c95ddd895d0] [formerly 4225ac80000f5f75e1731d44fbc2cfb15f57e364 [formerly 1d2178c73a64499f4811361f055c7ac3b5fff5ac]]
    Former-commit-id: 055697a7decfe261dd92a53101e47b6c6a780240 [formerly f5681e5056ff1d2990bf27394a18452e02dd2fa0]
    Former-commit-id: 8f198f7531c680aafcd551ad3257c825a21a741a
    Former-commit-id: ddde39ff8cfbfb372dbdee5441e89a3854bf3d02 [formerly d3ad0d1edc0e274ca65d967900b6cb3d32e579d2]
    Former-commit-id: c78eb59d49f9fc0e5ae124dce86ff3f6586fff26

--- scripts/odf_codes_plots.py
+++ scripts/odf_codes_plots.py
@@ -23,4 +23,10 @@ merge.ctd_residuals_df(df_bottle)
 df_bottle = merge.merge_ctd_codes(log_dir, df_bottle)
 df_coded = merge.get_qc_all_from_df(df_bottle)
 
+##### PATCHED IN FOR NBP1802 #####
+#df_coded = df_coded.rename(index=str, columns={'CTDCOND1_FLAG_W': 'CTDSAL_FLAG_W'})
+df_coded.to_pickle('data/values_and_codes.pkl')
+print(df_coded.axes)
+##### PATCHED IN FOR NBP1802 #####
+
 ctd_plots.all_plots(df_coded)
