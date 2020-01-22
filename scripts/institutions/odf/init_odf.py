965bda315706798b0a0444b79e04958ce238bd1f
Joseph Gum <jgum@ucsd.edu>
2020-01-22 13:36:45 -0800

    Creation of 'scripts/institutions' subdirectory for research group specific changes.
    Creation of 'init_odf.py' file to make data directories for startup.

---
+++ scripts/institutions/odf/init_odf.py
@@ -0,0 +1,13 @@
+'''
+This script will initialize the data directory for ctdcal according to ODF usage.
+Joseph Gum, January 22, 2019
+'''
+
+import pathlib
+
+data_directory = './data/'
+directories_to_be_made = ['raw', 'salt', 'bottle', 'converted', 'logs', 'oxygen', 'pressure', 'quality_codes', 'reft', 'time']
+
+#will make parents directory if not missing, will not throw an error if directory already exists
+for x in directories_to_be_made:
+    pathlib.Path(data_directory + x).mkdir(parents=True, exist_ok=True)
