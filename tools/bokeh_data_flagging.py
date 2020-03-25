8736a877a9f6c4ebcbc27a47dfad20b5d4bd4dce
mvkovatch3 <mkovatch@ucsd.edu>
2020-03-24 17:12:51 -0700

    added single ssscc plot and working on WAL for data exporting

--- tools/bokeh_data_flagging.py
+++ tools/bokeh_data_flagging.py
@@ -3,16 +3,16 @@ import numpy as np
 
 from bokeh.io import curdoc
 from bokeh.layouts import column, row
-from bokeh.plotting import figure, show
+from bokeh.plotting import figure
 from bokeh.models import (
     Button,
     ColumnDataSource,
     # CustomJS,
     DataTable,
-    Slider,
     TableColumn,
     Select,
     MultiSelect,
+    StringFormatter,  # https://docs.bokeh.org/en/latest/_modules/bokeh/models/widgets/tables.html#DataTable
 )
 
 
@@ -20,6 +20,7 @@ from bokeh.models import (
 df = pd.read_csv("../all_rinko_merged.csv")
 df["Residual"] = df["REFOXY_rinko"] - df["CTDRINKO"]
 df["Flag"] = 1
+df_edited = df.copy()
 ssscc_list = df["SSSCC_rinko"].unique()
 
 ### intialize widgets
@@ -30,11 +31,10 @@ parameter = Select(
     title="Parameter", options=["CTDTMP", "CTDCOND", "SBE43", "RINKO"], value="RINKO"
 )
 station = Select(title="Station", options=[str(x) for x in ssscc_list], value="00101")
-station.on_change("value", lambda attr, old, new: update())
 # explanation of flags:
 # https://cchdo.github.io/hdo-assets/documentation/manuals/pdf/90_1/chap4.pdf
 flag_list = MultiSelect(
-    title="Display data flagged as:",
+    title="Plot data flagged as:",
     value=["1", "2", "3"],
     options=[
         ("1", "1 [Uncalibrated]"),
@@ -43,63 +43,169 @@ flag_list = MultiSelect(
         ("4", "4 [Bad]"),
     ],
 )
+# returns list of select options, e.g., ['2'] or ['1','2']
 
 source_table = ColumnDataSource(data=dict())
-source_plot = ColumnDataSource(data=dict(x=[], y=[]))
+source_plot_ssscc = ColumnDataSource(data=dict(x=[], y=[]))
+source_plot_all = ColumnDataSource(data=dict(x=[], y=[]))
 
 ### set up plots
-x_var = "RINKO"
-ssscc = "00101"
-plot = figure(
+plot_ssscc = figure(
     plot_height=800,
-    plot_width=500,
-    title="{} vs CTDPRS [Station {}]".format(x_var, ssscc),
-    tools="crosshair,pan,reset,save,wheel_zoom",
+    plot_width=400,
+    title="{} vs CTDPRS [Station {}]".format(parameter.value, station.value),
+    tools="crosshair,pan,reset,wheel_zoom",
     y_axis_label="Pressure (dbar)",
 )
-plot.y_range.flipped = True  # invert y-axis
-plot.scatter(
+plot_all = figure(
+    plot_height=800,
+    plot_width=400,
+    title="{} vs CTDPRS [Station {}]".format(parameter.value, station.value),
+    tools="crosshair,reset,wheel_zoom",
+)
+plot_ssscc.y_range.flipped = True  # invert y-axis
+plot_all.y_range.flipped = True  # invert y-axis
+plot_all.scatter(df["REFOXY_rinko"], df["CTDPRS_rinko_ctd"])
+plot_ssscc.scatter(
     "x",
     "y",
     fill_color="#999999",
     line_color="#000000",
     size=10,
     line_width=2,
-    source=source_plot,
+    source=source_plot_ssscc,
 )
-plot.scatter(df["REFOXY_rinko"], df["CTDPRS_rinko_ctd"])
+plot_all.scatter(
+    "x",
+    "y",
+    fill_color="#999999",
+    line_color="#000000",
+    size=10,
+    line_width=2,
+    source=source_plot_all,
+)
+
+parameter.on_change("value", lambda attr, old, new: update())
+station.on_change("value", lambda attr, old, new: update())
+flag_list.on_change("value", lambda attr, old, new: update())
 
 
 def update():
-    current = df[df["SSSCC_rinko"] == int(station.value)]
-    source_table.data = {
-        "CTDPRS": current["CTDPRS_rinko_ctd"],
-        "REFOXY": current["REFOXY_rinko"],
-        "CTDRINKO": current["CTDRINKO"],
-        "diff": current["Residual"],
-        "flag": current["Flag"],
+
+    print("exec update()")
+
+    table_rows = df_edited["SSSCC_rinko"] == int(station.value)
+    plot_rows = (df_edited["SSSCC_rinko"] == int(station.value)) & (
+        df_edited["Flag"].isin(flag_list.value)
+    )
+
+    current_table = df_edited[table_rows]
+    current_plot = df_edited[plot_rows]
+
+    # can this be split off into separate updates? might improve speed
+    # update_param()
+    # update_station()
+    # update_plot_flag()
+    # update flag()
+
+    source_table.data = {  # this causes update_flag() to execute
+        "SSSCC": current_table["SSSCC_rinko"],
+        "CTDPRS": current_table["CTDPRS_rinko_ctd"],
+        "REFOXY": current_table["REFOXY_rinko"],
+        "CTDRINKO": current_table["CTDRINKO"],
+        "diff": current_table["Residual"],
+        "flag": current_table["Flag"],
     }
-    source_plot.data = {
-        "x": current["REFOXY_rinko"],
-        "y": current["CTDPRS_rinko_ctd"],
+    source_plot_ssscc.data = {
+        "x": current_plot["REFOXY_rinko"],
+        "y": current_plot["CTDPRS_rinko_ctd"],
     }
+    source_plot_all.data = {
+        "x": current_plot["REFOXY_rinko"],
+        "y": current_plot["CTDPRS_rinko_ctd"],
+    }
+    plot_ssscc.title.text = "{} vs CTDPRS [Station {}]".format(
+        parameter.value, station.value
+    )
+    plot_all.title.text = "{} vs CTDPRS [Station {}]".format(
+        parameter.value, station.value
+    )
+    plot_ssscc.x_range = plot_all.x_range
+    plot_ssscc.y_range = plot_all.y_range
+
+
+def update_flag():
+
+    print("exec update_flag()")
+
+    df_edited.loc[
+        df_edited["SSSCC_rinko"] == source_table.data["SSSCC"][0], "Flag",
+    ] = source_table.data["flag"]
 
 
 columns = [
-    TableColumn(field="CTDPRS", title="CTDPRS"),
-    TableColumn(field="REFOXY", title="REFOXY"),
-    TableColumn(field="CTDRINKO", title="CTDRINKO"),
-    TableColumn(field="diff", title="Residual"),
-    TableColumn(field="flag", title="Flag"),
+    TableColumn(
+        field="SSSCC",
+        title="SSSCC",
+        width=40,
+        formatter=StringFormatter(text_align="right"),
+    ),
+    TableColumn(
+        field="CTDPRS",
+        title="CTDPRS",
+        width=80,
+        formatter=StringFormatter(text_align="right"),
+    ),
+    TableColumn(
+        field="REFOXY",
+        title="REFOXY",
+        width=80,
+        formatter=StringFormatter(text_align="right"),
+    ),
+    TableColumn(
+        field="CTDRINKO",
+        title="CTDRINKO",
+        width=80,
+        formatter=StringFormatter(text_align="right"),
+    ),
+    TableColumn(
+        field="diff",
+        title="Residual",
+        width=80,
+        formatter=StringFormatter(text_align="right"),
+    ),
+    TableColumn(
+        field="flag",
+        title="Flag",
+        width=20,
+        formatter=StringFormatter(text_align="center", font_style="bold"),
+    ),
 ]
 
 data_table = DataTable(
-    source=source_table, columns=columns, width=500, height=800, editable=True,
+    source=source_table,
+    columns=columns,
+    index_width=20,
+    width=380 + 20,  # sum of col widths + idx width
+    height=800,
+    editable=True,
+    fit_columns=True,
+    sortable=False,
 )
+
+# TODO: build a second DataTable for points that have changed flag value
+
+# run update() when user selects new column (may indicate new flag value)
+# source_table.selected.on_change("indices", lambda attr, old, new: update_flag())
+source_table.on_change("data", lambda attr, old, new: update_flag())
+
+# TODO: highlight scatterpoint based on selected row
+# source_table.selected.on_change("indices", lambda attr, old, new: update_highlight())
+# source_table.selected.indices -> could likely be used to highlight point
 
 controls = column(parameter, station, flag_list, button, width=170)
 
-curdoc().add_root(row(controls, data_table, plot))
+curdoc().add_root(row(controls, data_table, plot_ssscc, plot_all))
 curdoc().title = "CTDO Data Flagging Tool"
 
 update()
