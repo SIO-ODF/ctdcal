073e813fe35278ad643ed7dbf82750e5f9782025
mvkovatch3 <mkovatch@ucsd.edu>
2020-04-01 17:05:07 -0700

    salt flagging now functional, including saving

--- tools/bokeh_ctd_data_flagging.py
+++ tools/bokeh_ctd_data_flagging.py
@@ -17,12 +17,15 @@ from bokeh.models import (
     Div,
 )
 
+# TODO: load in existing handcoded salts if it exists
+
 # load continuous CTD data and make into a dict (only ~20MB)
 file_list = sorted(glob.glob("../data/pressure/*.csv"))
 ssscc_list = [ssscc.strip("../data/pressure/")[:5] for ssscc in file_list]
 ctd_data = []
 for f in file_list:
     df = pd.read_csv(f, header=12, skiprows=[13], skipfooter=1, engine="python")
+    df["SSSCC"] = f.strip("../data/pressure/")[:5]
     ctd_data.append(df)
 ctd_data = pd.concat(ctd_data, axis=0, sort=False)
 
@@ -37,10 +40,9 @@ for f in file_list:
     df["SSSCC"] = f.strip("../data/salt/")[:5]
     salt_data.append(df)
 salt_data = pd.concat(salt_data, axis=0, sort=False)
+salt_data["SALNTY"] = salt_data["SALNTY"].round(4)
 if "SALNTY_FLAG_W" not in salt_data.columns:
     salt_data["SALNTY_FLAG_W"] = 2
-
-breakpoint()
 
 # load ctd btl data
 df_ctd_btl = pd.read_csv(
@@ -53,8 +55,7 @@ df_btl_all = pd.merge(df_ctd_btl, salt_data, on=["STNNBR", "CASTNO", "SAMPNO"])
 btl_data = df_btl_all.loc[
     :,
     [
-        "STNNBR",
-        "CASTNO",
+        "SSSCC",
         "SAMPNO",
         "CTDPRS",
         "CTDTMP",
@@ -64,17 +65,17 @@ btl_data = df_btl_all.loc[
         "SALNTY_FLAG_W",
     ],
 ]
-
-### TODO: pick up from here
+btl_data["Residual"] = btl_data["SALNTY"] - btl_data["CTDSAL"]
+btl_data[["CTDPRS", "Residual"]] = btl_data[["CTDPRS", "Residual"]].round(4)
+btl_data["Comments"] = ""
+btl_data["New Flag"] = btl_data["SALNTY_FLAG_W"].copy()
 
 
 # intialize widgets
 button = Button(label="Save flagged data", button_type="success")
-parameter = Select(
-    title="Parameter",
-    options=["CTDTMP", "CTDSAL", "CTDOXY", "CTDRINKO"],
-    value="CTDSAL",
-)
+parameter = Select(title="Parameter", options=["CTDSAL", "CTDTMP"], value="CTDSAL")
+ref_param = Select(title="Reference", options=["SALNTY"], value="SALNTY")
+# ref_param.options = ["foo","bar"]  # can dynamically change dropdowns
 station = Select(title="Station", options=ssscc_list, value="00101")
 # explanation of flags:
 # https://cchdo.github.io/hdo-assets/documentation/manuals/pdf/90_1/chap4.pdf
@@ -90,32 +91,26 @@ flag_list = MultiSelect(
 )
 # returns list of select options, e.g., ['2'] or ['1','2']
 
-source_table = ColumnDataSource(data=dict())
-source_table_changes = ColumnDataSource(data=dict())
-source_plot_ssscc = ColumnDataSource(data=dict(x=[], y=[]))
-source_plot_all = ColumnDataSource(data=dict(x=[], y=[]))
+# set up datasources
+src_table = ColumnDataSource(data=dict())
+src_table_changes = ColumnDataSource(data=dict())
+src_plot_ctd = ColumnDataSource(data=dict(x=[], y=[]))
+src_plot_btl = ColumnDataSource(data=dict(x=[], y=[]))
 
-### set up plots
+# set up plots
 plot_ssscc = figure(
     plot_height=800,
     plot_width=400,
     title="{} vs CTDPRS [Station {}]".format(parameter.value, station.value),
-    tools="crosshair,pan,reset,wheel_zoom",
+    tools="crosshair,pan,reset,box_zoom",
     y_axis_label="Pressure (dbar)",
 )
-plot_all = figure(
-    plot_height=800,
-    plot_width=400,
-    title="{} vs CTDPRS [Station {}]".format(parameter.value, station.value),
-    tools="crosshair,reset,wheel_zoom",
-)
 plot_ssscc.y_range.flipped = True  # invert y-axis
-plot_all.y_range.flipped = True  # invert y-axis
 plot_ssscc.line(
-    "x", "y", line_color="#000000", line_width=2, source=source_plot_ssscc,
+    "x", "y", line_color="#000000", line_width=2, source=src_plot_ctd,
 )
-plot_all.line(
-    "x", "y", line_color="#000000", line_width=2, source=source_plot_all,
+plot_ssscc.cross(
+    "x", "y", size=10, source=src_plot_btl,
 )
 
 parameter.on_change("value", lambda attr, old, new: update_selectors())
@@ -126,62 +121,81 @@ flag_list.on_change("value", lambda attr, old, new: update_selectors())
 def update_selectors():
 
     print("exec update_selectors()")
+    ctd_rows = ctd_data["SSSCC"] == station.value
+    table_rows = btl_data["SSSCC"] == station.value
+    btl_rows = (btl_data["SALNTY_FLAG_W"].isin(flag_list.value)) & (
+        btl_data["SSSCC"] == station.value
+    )
 
-    df_edited = ctd_dict[station.value]
+    # update table data
+    current_table = btl_data[table_rows].reset_index()
+    src_table.data = {  # this causes update_flag() to execute
+        "SSSCC": current_table["SSSCC"],
+        "CTDPRS": current_table["CTDPRS"],
+        "CTDSAL": current_table["CTDSAL"],
+        "SALNTY": current_table["SALNTY"],
+        "diff": current_table["Residual"],
+        "flag": current_table["New Flag"],
+        "comments": current_table["Comments"],
+    }
 
-    # breakpoint()
-    flag_col = parameter.value + "_FLAG_W"
-    rows = df_edited[flag_col].isin(flag_list.value)
-    source_plot_ssscc.data = {
-        "x": df_edited.loc[rows, parameter.value],
-        "y": df_edited.loc[rows, "CTDPRS"],
+    # update plot data
+    src_plot_btl.data = {
+        "x": btl_data.loc[btl_rows, parameter.value],
+        "y": btl_data.loc[btl_rows, "CTDPRS"],
     }
-    source_plot_all.data = {
-        "x": df_edited.loc[rows, parameter.value],
-        "y": df_edited.loc[rows, "CTDPRS"],
+    src_plot_ctd.data = {
+        "x": ctd_data.loc[ctd_rows, parameter.value],
+        "y": ctd_data.loc[ctd_rows, "CTDPRS"],
     }
+
+    # update plot labels/axlims
     plot_ssscc.title.text = "{} vs CTDPRS [Station {}]".format(
         parameter.value, station.value
     )
-    plot_all.title.text = "{} vs CTDPRS [Station {}]".format(
-        parameter.value, station.value
-    )
-    plot_ssscc.x_range = plot_all.x_range
-    plot_ssscc.y_range = plot_all.y_range
     plot_ssscc.xaxis.axis_label = parameter.value
-    plot_all.xaxis.axis_label = parameter.value
 
 
+# TODO: add bulk flag/comment updates
 def update_flag():
 
     print("exec update_flag()")
 
     # breakpoint()
 
-    df_edited.loc[
-        df_edited["SSSCC_rinko"] == source_table.data["SSSCC"].values[0], "Flag New",
-    ] = source_table.data["flag"].values
-    df_edited.loc[
-        df_edited["SSSCC_rinko"] == source_table.data["SSSCC"].values[0], "Comments",
-    ] = source_table.data["comments"].values
+    btl_data.loc[
+        btl_data["SSSCC"] == src_table.data["SSSCC"].values[0], "New Flag",
+    ] = src_table.data["flag"].values
+    btl_data.loc[
+        btl_data["SSSCC"] == src_table.data["SSSCC"].values[0], "Comments",
+    ] = src_table.data["comments"].values
 
-    edited_rows = (df_edited["Flag"] - df_edited["Flag New"]) != 0
+    edited_rows = (btl_data["New Flag"].isin([3, 4])) | (btl_data["Comments"] != "")
 
-    source_table_changes.data = {
-        "SSSCC": df_edited.loc[edited_rows, "SSSCC_rinko"],
-        "CTDPRS": df_edited.loc[edited_rows, "CTDPRS_rinko_ctd"],
-        "diff": df_edited.loc[edited_rows, "Residual"],
-        "flag_old": df_edited.loc[edited_rows, "Flag"],
-        "flag_new": df_edited.loc[edited_rows, "Flag New"],
-        "comments": df_edited.loc[edited_rows, "Comments"],
+    src_table_changes.data = {
+        "SSSCC": btl_data.loc[edited_rows, "SSSCC"],
+        "SAMPNO": btl_data.loc[edited_rows, "SAMPNO"],
+        "diff": btl_data.loc[edited_rows, "Residual"],
+        "flag_old": btl_data.loc[edited_rows, "SALNTY_FLAG_W"],
+        "flag_new": btl_data.loc[edited_rows, "New Flag"],
+        "comments": btl_data.loc[edited_rows, "Comments"],
     }
 
 
 def save_data():
 
-    print("Save button clicked...")
+    print("Saving flagged data...")
 
-    # df_out =
+    # get data from table
+    df_out = pd.DataFrame.from_dict(src_table_changes.data)
+
+    # minor changes to columns/names/etc.
+    df_out = df_out.rename(columns={"flag_new": "salinity_flag"}).drop(
+        columns="flag_old"
+    )
+
+    # save it
+    df_out.to_csv("salt_flags_handcoded.csv", index=None)
 
 
 button.on_click(save_data)
@@ -201,14 +215,14 @@ columns = [
         formatter=StringFormatter(text_align="right"),
     ),
     TableColumn(
-        field="REFOXY",
-        title="REFOXY",
+        field="CTDSAL",
+        title="CTDSAL",
         width=80,
         formatter=StringFormatter(text_align="right"),
     ),
     TableColumn(
-        field="CTDRINKO",
-        title="CTDRINKO",
+        field="SALNTY",
+        title="SALNTY",
         width=80,
         formatter=StringFormatter(text_align="right"),
     ),
@@ -239,9 +253,9 @@ columns_changed = [
         formatter=StringFormatter(text_align="right"),
     ),
     TableColumn(
-        field="CTDPRS",
-        title="CTDPRS",
-        width=80,
+        field="SAMPNO",
+        title="Bottle",
+        width=40,
         formatter=StringFormatter(text_align="right"),
     ),
     TableColumn(
@@ -267,12 +281,12 @@ columns_changed = [
     TableColumn(
         field="comments",
         title="Comments",
-        width=100,
+        width=140,
         formatter=StringFormatter(text_align="left"),
     ),
 ]
 data_table = DataTable(
-    source=source_table,
+    source=src_table,
     columns=columns,
     index_width=20,
     width=480 + 20,  # sum of col widths + idx width
@@ -282,7 +296,7 @@ data_table = DataTable(
     sortable=False,
 )
 data_table_changed = DataTable(
-    source=source_table_changes,
+    source=src_table_changes,
     columns=columns_changed,
     index_width=20,
     width=480 + 20,  # sum of col widths + idx width
@@ -296,18 +310,18 @@ data_table_changed_title = Div(text="""<b>Edited Data:</b>""", width=200, height
 
 # run update() when user selects new column (may indicate new flag value)
 # source_table.selected.on_change("indices", lambda attr, old, new: update_flag())
-# source_table.on_change("data", lambda attr, old, new: update_flag())
+src_table.on_change("data", lambda attr, old, new: update_flag())
 
 # TODO: highlight scatterpoint based on selected row
 # source_table.selected.on_change("indices", lambda attr, old, new: update_highlight())
 # source_table.selected.indices -> could likely be used to highlight point
 
-controls = column(parameter, station, flag_list, button, width=170)
+controls = column(parameter, ref_param, station, flag_list, button, width=170)
 tables = column(
     data_table_title, data_table, data_table_changed_title, data_table_changed
 )
 
-curdoc().add_root(row(controls, tables, plot_ssscc, plot_all))
+curdoc().add_root(row(controls, tables, plot_ssscc))
 curdoc().title = "CTDO Data Flagging Tool"
 
 update_selectors()
