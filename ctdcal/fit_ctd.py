d9bb68ea83c2f153e9fa513361b53eb74d22a967
mvkovatch3 <mkovatch@ucsd.edu>
2021-08-31 17:08:37 -0700

    fully flesh out multivariate fit function

--- ctdcal/fit_ctd.py
+++ ctdcal/fit_ctd.py
@@ -261,38 +261,79 @@ def _get_T_coefs(df, T_col=None, P_order=2, T_order=2, zRange=None, f_stem=None)
     return coefs, df_bad
 
 
-def multivariate_fit(y, *args):
+def multivariate_fit(y, *args, coef_names=None, const_name="c0"):
     """
-    Least-squares fit data using multiple dependent variables.
+    Least-squares fit data using multiple dependent variables. Dependent variables must
+    be provided in tuple pairs of (data, order) as positional arguments.
+
+    If coef_names are defined, coefficients will be returned as a dict. Otherwise,
+    coefficients are return as an array in the order of the dependent variables, sorted
+    by decreasing powers.
 
     Parameters
     ----------
     y : array-like
         Indepedent variable to be fit
     args : tuple
-        Pairs of fit order and data for dependent variables (i.e., (order, data))
+        Pairs of dependent variable data and fit order (i.e., (data, order))
+    coef_names : list-like, optional
+        Base names for coefficients (i.e., "a" for 2nd order yields ["a2", "a1"])
+    const_name : str, optional
+        Name for constant offset term
 
     Returns
     -------
     coefs : array-like
         Least-squares fit coefficients in decreasing powers
+
+    Examples
+    --------
+    Behavior when coef_names is None:
+
+    >>> z = [1, 4, 9]
+    >>> x = [1, 3, 5]
+    >>> y = [1, 2, 3]
+    >>> multivariate_fit(z, (x, 2), (y, 1))
+    array([0.25, 0.375, 0.25, 0.125])  # [c1, c2, c3, c4]
+
+    where z = (c1 * x ** 2) + (c2 * x) + (c3 * y) + c4
+
+    Behavior when coef_names is given:
+
+    >>> z = [1, 4, 9]
+    >>> x = [1, 3, 5]
+    >>> y = [1, 2, 3]
+    >>> multivariate_fit(z, (x, 2), (y, 1), coef_names=["a", "b"])
+    {"a2": 0.25, "a1": 0.375, "b1": 0.25, "c0": 0.125}
+
+    where z = (a2 * x ** 2) + (a1 * x) + (b1 * y) + c0
     """
-    rows = []
-    for arg in args:
+    to_dict = True
+    if coef_names is None:
+        to_dict = False
+        coef_names = [""] * len(args)  # needed to prevent zip() error
+    elif len(args) != len(coef_names):
+        raise ValueError("coef_names must be the same length as dependent variables")
+
+    # iteratively build fit matrix
+    rows, names = [], []
+    for arg, coef_root in zip(args, coef_names):
         if type(arg) is not tuple:
             raise TypeError(f"Positional args must be tuples, not {type(arg)}")
-        # if len(arg) != 2:
-        #     raise ValueError("dependent variable tuples must be length 2")
 
-        order, series = arg
+        series, order = arg
         for n in np.arange(1, order + 1)[::-1]:
             rows.append(series ** n)  # n is np.int64 so series will cast to np.ndarray
+            names.append(f"{coef_root}{n}")
 
+    # add constant offset term
     rows.append(np.ones(len(y)))
+    names.append(const_name)
+
     fit_matrix = np.vstack(rows)
     coefs = np.linalg.lstsq(fit_matrix.T, y, rcond=None)[0]
 
-    return coefs
+    return dict(zip(names, coefs)) if to_dict else coefs
 
 
 def calibrate_temp(btl_df, time_df):
@@ -367,8 +408,9 @@ def calibrate_temp(btl_df, time_df):
         )
         new_coefs = multivariate_fit(
             df_good["Diff"],
-            (fit_yaml.fit_orders1[f_stem]["P_order"], df_good[cfg.column["p"]]),
-            (fit_yaml.fit_orders1[f_stem]["T_order"], df_good[cfg.column["t1"]]),
+            (df_good[cfg.column["p"]], fit_yaml.fit_orders1[f_stem]["P_order"]),
+            (df_good[cfg.column["t1"]], fit_yaml.fit_orders1[f_stem]["T_order"]),
+            coef_names=["cp", "ct"],
         )
         coef_t1, df_bad_t1 = _get_T_coefs(
             btl_df[good_rows],
