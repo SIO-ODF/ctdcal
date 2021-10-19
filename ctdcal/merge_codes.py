3006d64b470ffa765dcbdbbb3d9d491a1ebe2957
mvkovatch3 <mkovatch@ucsd.edu>
2021-10-19 11:30:14 -0700

    remove 'Courtney style' helper functions

--- ctdcal/merge_codes.py
+++ ctdcal/merge_codes.py
@@ -1,116 +1,9 @@
-import os
-import sys
-
 import gsw
 import numpy as np
 import pandas as pd
 
 """code_pruning: is there anything worth keeping in this module?
 only imported in outdated scripts"""
-
-
-def string_converter(value):
-    """To deal with Courtney CTD codes"""
-    return value.split(":")[-1].strip()
-
-
-def int_converter(value):
-    """To deal with Courtney CTD codes"""
-    return int(float(value.split(":")[-1].strip()))
-
-
-def float_converter(value):
-    """To deal with Courtney CTD codes"""
-    return float(value.split(":")[-1])
-
-
-def merge_courtney_ctd_codes(bottle_df, qc_df):
-    """Take in ctd bottle file to be given to database, qc logs, and combine together.
-    For use with Courtney style codes (P06W 2017).
-    Input:
-    bottle_file - dataframe
-    qc_file - dataframe
-
-    Output:
-    b_test - dataframe with updated codes
-    """
-
-    flags = {
-        "T": "REFTMP_FLAG_W",
-        "T1": "CTDTMP1_FLAG_W",
-        "T2": "CTDTMP2_FLAG_W",
-        "C": "SALNTY_FLAG_W",
-        "C1": "CTDCOND1_FLAG_W",
-        "C2": "CTDCOND2_FLAG_W",
-    }
-
-    index = ["STNNBR", "CASTNO", "SAMPNO"]
-
-    ### too lazy to fix it all in jupyter, do later
-    a = qc_df
-    b = bottle_df
-
-    ### setup indicies for writing to
-    a["CASTNO"] = a["stacast"] % 100
-    a["STNNBR"] = (a["stacast"] - a["CASTNO"]) // 100
-    a["new_params"] = None
-
-    ### update new_params to flag_parameter for df.groupby() later
-    for param, flag_param in flags.items():
-        a.loc[a["parameter"] == param, "new_params"] = flag_param
-    a_gb = a.groupby("new_params")
-
-    ### df.update() requires the index to be set before being called
-    b_test = b.set_index(index)
-
-    ### loop through all flag_parameter names
-    for param, flag_param in flags.items():
-        try:
-            a_gb_project = a_gb.get_group(flag_param).copy()
-            a_gb_project.rename(columns={"flag": flag_param}, inplace=True)
-
-            ### df.update() requires the index to be set before being called
-            a_test = a_gb_project[index + [flag_param]].set_index(index)
-
-            ### df.update() returns inplace, so return signature = None
-            b_test.update(a_test)
-        except KeyError:
-            print(f"Parameter not found: {flag_param}")
-
-    ### reset to work with again
-    b_test = b_test.reset_index()
-
-    return b_test
-
-
-def load_courtney_ctd_codes(qc_file):
-    """Load Courtney style ctd codes, and pass out dataframe.
-    Input:
-    qc_file - filepath to a courtney style ctd code
-    Output:
-    dataframe
-    """
-    converters = {
-        0: int_converter,
-        1: int_converter,
-        2: string_converter,
-        3: float_converter,
-        4: int_converter,
-        5: float_converter,
-        6: float_converter,
-        7: float_converter,
-    }
-    names = [
-        "stacast",
-        "SAMPNO",
-        "parameter",
-        "pressure",
-        "flag",
-        "primary_diff",
-        "secondary_diff",
-        "p_minus_s",
-    ]
-    return pd.read_csv(qc_file, header=None, converters=converters, names=names)
 
 
 def load_to_odf_db_bottle_file(bottle_file):
@@ -184,27 +77,6 @@ def write_to_odf_db_bottle_file(df_bottle, output_file):
     with open(output_file, "w") as f_handle:
         f_handle.write(stringy)
     return None
-
-
-def merge_ctd_codes(log_folder, df_bottle):
-    """
-    Chopped version of old merge_ctd_codes to deal with dfs
-    Input:
-    log_folder - file path
-    bottle_file - df
-
-    Output:
-    btl -  dataframe
-    """
-    ### Expecting all codes to be in one directory for now. Must change later
-    log_files = os.listdir(log_folder)
-
-    ### Merge quality codes, and store a copy in-memory in btl
-    for f in log_files:
-        x = load_courtney_ctd_codes(f"{log_folder}{f}")
-        df_bottle = merge_courtney_ctd_codes(df_bottle, x)
-
-    return df_bottle
 
 
 def merge_bottle_trip_dfs(file_ssscc):
@@ -440,29 +312,3 @@ def residual_stddev(
         df_deep = df[df["CTDPRS"] > 2000]
         output[f"{x}_DEEP"] = df_deep[f"{x}"].std() * 2
     return output
-
-
-def main(argv):
-    """Example run"""
-    qual_codes_filepath = "data/quality_codes/"
-    cruise_dir = "data/"
-    log_dir = "data/logs"
-
-    file_ssscc = f"{cruise_dir}ssscc.csv"
-    bottle_file = f"{qual_codes_filepath}320620180309_hy1.csv"
-
-    ### compile bottle trips into one file, then merge with odf_db bottle file
-    df_bottle = prelim_ctd_bottle_df(file_ssscc, bottle_file)
-    ### compute residuals for plotting
-    ctd_residuals_df(df_bottle)
-    load_courtney_ctd_codes()
-    df_bottle = merge_ctd_codes(log_dir, df_bottle)
-    df_bottle.to_pickle(f"{qual_codes_filepath}merged_ctd_codes.pkl")
-
-    # df_coded = get_qc_all_from_df(df_bottle)
-    ### write file out
-    return None
-
-
-if __name__ == "__main__":
-    main(sys.argv[1:])
