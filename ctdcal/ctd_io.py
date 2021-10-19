f38a59301cddb60912c8c2fbe602ecd32f92af9d
mvkovatch3 <mkovatch@ucsd.edu>
2021-10-19 15:52:15 -0700

    clean up docstrings, initialize mypy

--- ctdcal/ctd_io.py
+++ ctdcal/ctd_io.py
@@ -34,27 +34,29 @@ def load_exchange_btl(btl_file: Union[str, Path]) -> pd.DataFrame:
     )
 
 
-def write_pressure_details(stacast, log_file, start, end):
+def write_pressure_details(
+    ssscc: str, log_file: Union[str, Path], p_start: float, p_end: float
+) -> None:
     """
     Write start/end deck pressure to ondeck_pressure.csv log file.
 
     Parameters
     ----------
-    stacast : str
-        station cast data for file
-    c_file : str
-        file name location for cast details
-    start : str
-        cast start time from top of cast after 10m soak
-    end : str
-        cast end time when instrument leaves water
+    ssscc : str
+        Station/cast in SSSCC format
+    log_file : str or Path
+        File destination for pressure details
+    p_start : float
+        Average starting on-deck pressure (pre-deployment)
+    p_end : float
+        Average ending on-deck pressure (post-deployment)
 
     Returns
     -------
     None
     """
     df = pd.DataFrame(
-        {"SSSCC": stacast, "ondeck_start_p": start, "ondeck_end_p": end}, index=[0]
+        {"SSSCC": ssscc, "ondeck_start_p": p_start, "ondeck_end_p": p_end}, index=[0]
     )
     add_header = not Path(log_file).exists()  # add header iff file doesn't exist
     with open(log_file, "a") as f:
@@ -62,33 +64,42 @@ def write_pressure_details(stacast, log_file, start, end):
 
 
 def write_cast_details(
-    stacast, c_file, start, end, bottom, start_p, max_p, b_alt, b_lat, b_lon
-):
+    ssscc: str,
+    log_file: Union[str, Path],
+    time_start: float,
+    time_end: float,
+    time_bottom: float,
+    p_start: float,
+    p_max: float,
+    b_alt: float,
+    b_lat: float,
+    b_lon: float,
+) -> None:
     """
     Write cast details to cast_details.csv log file.
 
     Parameters
     ----------
-    stacast : str
-        station cast data for file
-    c_file : str
-        file name location for cast details
-    start : str
-        cast start time from top of cast after 10m soak
-    end : str
-        cast end time when instrument leaves water
-    bottom : str
-        bottom of cast time when instrument reaches max depth
-    start_p : str
-        starting pressure at the time the cast begins
-    max_p : str
-        maximum pressure for entire cast
-    b_alt : str
-        altimeter value at bottom of cast
-    b_lat : str
-        latitude at bottom of cast
-    b_lon : str
-        longitude at bottom of cast
+    ssscc : str
+        Station/cast in SSSCC format
+    log_file : str or Path
+        File destination for cast details
+    time_start : float
+        Time at start of cast (from minimum pressure after 10m soak)
+    time_end : float
+        Time at end of cast (when instrument leaves water)
+    time_bottom : float
+        Time at bottom of cast (max depth)
+    p_start : float
+        Pressure at the time the cast begins
+    p_max : float
+        Pressure at bottom of cast
+    b_alt : float
+        Altimeter value at bottom of cast
+    b_lat : float
+        Latitude at bottom of cast
+    b_lon : float
+        Longitude at bottom of cast
 
     Returns
     -------
@@ -96,18 +107,18 @@ def write_cast_details(
     """
     df = pd.DataFrame(
         {
-            "SSSCC": stacast,
-            "start_time": start,
-            "bottom_time": bottom,
-            "end_time": end,
-            "start_pressure": start_p,
-            "max_pressure": max_p,
+            "SSSCC": ssscc,
+            "start_time": time_start,
+            "bottom_time": time_bottom,
+            "end_time": time_end,
+            "start_pressure": p_start,
+            "max_pressure": p_max,
             "altimeter_bottom": b_alt,
             "latitude": b_lat,
             "longitude": b_lon,
         },
         index=[0],
     )
-    add_header = not Path(c_file).exists()  # add header iff file doesn't exist
-    with open(c_file, "a") as f:
+    add_header = not Path(log_file).exists()  # add header iff file doesn't exist
+    with open(log_file, "a") as f:
         df.to_csv(f, mode="a", header=add_header, index=False)
