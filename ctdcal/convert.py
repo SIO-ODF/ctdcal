915292f506d143c8f5787740df21d505e2cf2bf0
Klankers <aaronmau40@gmail.com>
2022-05-14 00:32:30 +1100

    01701 remove last btl_fire series

--- ctdcal/convert.py
+++ ctdcal/convert.py
@@ -138,11 +138,19 @@ def hex_to_ctd(ssscc_list):
             breakpoint
             converted_df = convertFromSBEReader(sbeReader, ssscc)
 
-            if ssscc == "00801":    #   P02: Evolve this into import NaN check and determine which sensor is "reliable" enough for SAL/OXY calculations
+            if ssscc == "00801":    #   T1 failed #   P02: Evolve this into import NaN check and determine which sensor is "reliable" enough for SAL/OXY calculations
                 converted_df["CTDTMP1"] = converted_df["CTDTMP2"]
                 print("***Using T2 as primary T for cast " + ssscc + "***")
-            elif ssscc == "00904":
+            elif ssscc == "00904":  #   Cast w/o T2
                 converted_df["CTDTMP2"] = np.nan
+            elif ssscc == "01701":  #   "Fire bottle" accidently pressed after 36: Maybe do check like this (#fired bottles > 36) if last index is greater than 95% of the way through the cast?
+                a = pd.DataFrame()
+                a['btl_fire_num'] = (((converted_df['btl_fire'])&(converted_df['btl_fire']!=converted_df['btl_fire'].shift(1))).astype(int).cumsum())
+                a = a['btl_fire_num'].diff()    #   boolean
+                a = a.index[a == 1].tolist()    #   indices where bottles were fired in continuous file
+                converted_df["btl_fire"].iloc[a[-1]:] = False
+                print("***Bottle was accidently fired 37 times during " + ssscc + "***")
+                
             converted_df.to_pickle(cfg.dirs["converted"] + ssscc + ".pkl")
 
     return True
