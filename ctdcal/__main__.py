e1b41a7600d219c1ebf3ac156f7767ea96fb024e
Klankers <aaronmau40@gmail.com>
2022-05-16 01:53:19 +1100

    Can't compare branches?

--- ctdcal/__main__.py
+++ ctdcal/__main__.py
@@ -1,7 +1,9 @@
+from distutils import filelist
 from pathlib import Path
 
 import click
 import logging
+import time
 from . import get_ctdcal_config
 
 # Rich handling
@@ -55,6 +57,33 @@ def init():
     shutil.copy2("fit_coefs.yaml", "./data/logs/fit_coefs.yaml")
     shutil.copy2("o2flasks.vol", "./data/oxygen/o2flasks.vol")
 
+@cli.command()
+def offload():
+    """Move everything that was processed/non-essential out for a fresh ctdcal run"""
+
+    import os
+    import shutil
+    import glob
+
+    folder = "".join(map(str, time.localtime()[0:6])) + "/"  #   Create a distinct folder from current date and time
+    Path(folder).mkdir()
+
+    #   List subfolders of data
+    subfolderlist = glob.glob('data/*/')
+    for f in subfolderlist:
+        #   Create list of files in each folder
+        h = f + '*.csv'
+        i = f + '*.pkl'
+        csv = glob.glob(h)
+        pkl = glob.glob(i)
+        #   Now use shutil.move to grab each csv in csv from f and every pkl in pkl from f
+        # filelist = os.listdir(f)
+        # for ff in filelist:
+            
+    #   
+
+    log.info(f"")
+
 @cli.command("import")  # click workaround to get a command named 'import'
 def import_data():
     """Import data from given folder into ctdcal for processing"""
@@ -80,6 +109,7 @@ def import_data():
 def process(group, type):
     """Process data using a particular group's methodology"""
 
+    t = time.time()
     if group == "ODF":
         from .scripts.odf_process_all import odf_process_all
 
@@ -87,7 +117,8 @@ def process(group, type):
     elif group == "PMEL":
         # pmel_process()
         pass
-
+    elapsed = time.time() - t
+    log.info("Processing complete: " + str(round(elapsed/60)) + " minutes and " + str(round(elapsed)) + " seconds.")
 
 @cli.command()
 def cruise_report():
@@ -97,6 +128,28 @@ def cruise_report():
 
     cruise_report_residuals()
 
+@cli.command()  #   MK
+def qc():  # pragma: no cover
+    from importlib import resources
+    from bokeh.application import Application
+    from bokeh.application.handlers.script import ScriptHandler
+    from bokeh.server.server import Server
+    from tornado.ioloop import IOLoop
+    """Launch interactive data flagging web app for QA/QC"""
+    io_loop = IOLoop.current()
+    with resources.path("ctdcal.tools", "data_qc.py") as fname:
+        bokeh_app = Application(ScriptHandler(filename=fname))
+    server = Server(bokeh_app, io_loop=io_loop)
+    server.start()
+    server.show("/")
+    io_loop.start()
+
+@cli.command()
+def vis():
+    """Launch data_vis"""
+    from .tools import data_vis
+
+    data_vis  #   ...can I just run it?
 
 if __name__ == "__main__":
     cli()
