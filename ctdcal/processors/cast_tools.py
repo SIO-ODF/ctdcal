ce26dc1ed0735ac25038e0e051f496f931ca2163
Allen Smith <23222977+asmith75218@users.noreply.github.com>
2024-08-30 09:26:46 -0700

    Adding logging, improving docstrings

--- ctdcal/processors/cast_tools.py
+++ ctdcal/processors/cast_tools.py
@@ -1,6 +1,7 @@
 """
 Tools for cleaning up converted and processed cast data.
 """
+import logging
 from pathlib import Path
 
 import pandas as pd
@@ -8,16 +9,36 @@ import numpy as np
 from scipy import signal as sig
 
 
+log = logging.getLogger(__name__)
+
+
 class Cast(object):
     """
     Cast data container with methods for separating upcast, filtering
     and trimming soak period.
+
+    Attributes
+    ----------
+    cast_id : str
+        Cast identifier.
+    datadir : str or Path-like
+        Data directory.
+    p_col : str
+        Name of pressure column.
+    proc : DataFrame
+        Pre-processed cast data.
+    downcast : DataFrame
+        Downast data trimmed from full cast data.
+    filtered : array-like
+        Filtered cast data.
+    trimmed : DataFrame
+        Upcast data trimmed of initial soak data.
     """
     def __init__(self, cast_id, datadir):
-        self.cast_id = cast_id  # cast identifier
-        self.datadir = datadir  # data directory
-        self.p_col = None  # name of pressure column
-        self.proc = None  # processed cast data
+        self.cast_id = cast_id
+        self.datadir = datadir
+        self.p_col = None
+        self.proc = None
         self.downcast = None
         self.filtered = None
         self.trimmed = None
@@ -38,10 +59,10 @@ class Cast(object):
         Parameters
         ----------
         data : DataFrame
-            cast data
+            Cast data.
         """
         if self.p_col is None:
-            raise AttributeError("Pressure column (p_col) attribute is not set")
+            raise AttributeError("Pressure column 'p_col' attribute is not set")
         downcast = data.loc[:self.proc[self.p_col].idxmax()].copy()
         self.downcast = downcast
 
@@ -52,14 +73,14 @@ class Cast(object):
 
         Parameters
         ----------
-        data : DataFrame, array-like
-            cast data
+        data : array-like
+            Cast data.
         win_size : int
-            window size in number of samples
+            Window size in number of samples.
         win_type : str
-            window type
+            Window type.
         cols : list
-            column names to filter
+            Column names to filter.
         """
         data = data.copy()
         if win_type == "boxcar":
@@ -80,16 +101,16 @@ class Cast(object):
         cast data, and choosing the last one above the threshold.
 
         data : DataFrame
-            cast data
+            Downcast data.
         win_size : int
-            window size in number of samples
+            Window size in number of samples.
         max_soak : int
-            soak threshold in cast pressure units
+            Soak threshold in cast pressure units.
         """
         if self.p_col is None:
-            raise AttributeError("Pressure column (p_col) attribute is not set")
+            raise AttributeError("Pressure column 'p_col' attribute is not set")
         if self.downcast is None:
-            raise AttributeError("Downcast attribute is not set.")
+            raise AttributeError("'downcast' attribute is not set.")
         if win_size < 0:
             win_size = 0  # n must be positive or zero
 
@@ -103,7 +124,7 @@ class Cast(object):
             try:
                 local_min_vals.drop(i, inplace=True)
             except KeyError:
-                print('Whoa, trouble finding the soak on cast %s! Nothing was trimmed!' % self.cast_id)
+                log.warning('Whoa, trouble finding the soak on cast %s! Nothing was trimmed!' % self.cast_id)
                 return
             i = local_min_vals.index[-1]
 
